9b64a02404a06a5958ecd814d75eee861380f367
Allan <amaxwellblair@gmail.com>
2016-02-28 18:35:51 -0700

    Add functionality for a non-localhost

--- chit-chat/main.go
+++ chit-chat/main.go
@@ -11,13 +11,19 @@ import (
 )
 
 func main() {
-	clientPtr := flag.Bool("client", false, "bar")
-	replPtr := flag.Bool("repl", false, "bar")
+	client := flag.Bool("client", false, "CLIENT recieves messaages from other chit-chat clients")
+	repl := flag.Bool("repl", false, "REPL allows you send messages")
+	ip := flag.String("ip", "", "IP is the address Client and REPL use to communicate to the server")
 	flag.Parse()
 
-	if *clientPtr == true {
+	if *ip == "" {
+		fmt.Println("You have not entered in an ip address using the flag -ip")
+		return
+	}
+
+	if *client == true {
 		for {
-			response, err := http.Get("http://localhost:9000/chat")
+			response, err := http.Get("http://" + *ip + ":9000/chat")
 			if err != nil {
 				fmt.Println(err.Error())
 			}
@@ -29,14 +35,14 @@ func main() {
 		}
 	}
 
-	if *replPtr == true {
+	if *repl == true {
 		for {
 			reader := bufio.NewReader(os.Stdin)
 			fmt.Print("Enter text: ")
 			text, _ := reader.ReadString('\n')
 			v := url.Values{}
 			v.Add("body", text)
-			_, _ = http.PostForm("http://localhost:9000/", v)
+			_, _ = http.PostForm("http://"+*ip+":9000/", v)
 		}
 	}
 }
